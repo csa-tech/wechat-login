87d0f62381c79b106def5fbfc5d992e887dfe49f
yumign <35503192+yumign@users.noreply.github.com>
2019-03-06 23:24:09 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,150 @@
+
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var pageview = require('./routes/pageview');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/pageview', pageview);
+
+app.use('/test', router)
+
+const request = require("request");
+const use = {};
+use.open = function (opts) {
+    return new Promise(function (resolve, reject) {
+        request(opts, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                resolve(body);
+            } else {
+                reject();
+            }
+        })
+    });
+}
+//module.exports = use;
+
+//路由
+// router.get('/', urlencodedParser, (req, res) => {
+//         //拿到前台给的code后，发送请求
+//     if(req.body.code) {
+//         let options = {
+//             method: 'GET',
+//             url: 'https://api.weixin.qq.com/sns/jscode2session?',
+//             formData: {
+//             //     appid:wxdf4236783aec3090,
+//             //     //secret: 9cfaa5e0f39a51f480f20b72c402b599,
+//             //     js_code: req.body.code,
+//             //     grant_type: 'authorization_code'
+//                 UserImgUrl: "jimeone",
+//                 carImgUrl: "jimeone",
+//                 name: "jimeone",
+//                 carType: "jimeone",
+//                 carLicense: "jimeone",
+//                 carColor: "jimeone"
+//             }
+//         }
+//     }        
+// })
+router.get('/', (req, res, next) => {
+    //获取openid并返回
+    //router.login = function (req, res) {
+    console.log(req)
+    let r1 = ' ';
+    let appId = 'wxdf4236783aec3090';
+    let secret = '9cfaa5e0f39a51f480f20b72c402b599';
+    let js_code = req.body.code;
+    let encryptedData = req.body.encryptedData;
+    let iv = req.body.iv;
+    let sessionKey = ' ';
+    let opts = {
+        url: `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${secret}&js_code=${js_code}&grant_type=authorization_code`
+
+    }
+    use.open(opts).then(function (value) {
+        var r1 = JSON.parse(value)
+        var pc = new WXBizDaraCrypt(appId, r1.session_key)
+        var data = pc.decryptData(encryptedData, iv)
+        console.log('Decrypted',data)
+    }, function (error) {
+        console.log(error)
+    });
+        
+        
+        
+    //连接数据库并查找是否存在
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host     : 'mydatabase.c9ukuxyqda4n.us-west-1.rds.amazonaws.com',
+        port     : '3306',
+        database : 'rideshare',
+        multipleStatements: true,
+        user     : 'CSAUser',
+        password : 'Csa666!!',
+    });
+
+    connection.connect(function(err) {
+        if(err) console.log('与MySQL数据库建立连接失败。');
+        else{
+            console.log('与MySQL数据库建立连接成功。');
+            connection.query('SELECT * FROM user_info WHERE wechat_openid=\'100\'',function(err,data) {               //wechat_openid 记得修改
+                           if(err) {
+                               console.log('查询数据失败');
+                               res.status(404).send('查询数据失败');
+                               
+                           } else {
+                               console.log(data)
+                               if(data.length==0){
+                                   var add = 'INSERT INTO user_info(wechat_openid,user_id) VALUES(\'fym\',' +Math.round(Math.random()*100000000000)+ ')';     //暂时是undefined 还没收到后端 ' +req.body.code + '
+                                  
+                                   
+                                   connection.query(add,function(er,result) {
+                                        if(er){
+                                                console.log(er);
+                                                console.log('插入数据失败');
+                                                res.status(404).send('插入数据失败');
+                                        } else {
+                                                    if(result.length==0){
+                                       console.log('插入数据失败');
+                                   } else {
+                                       console.log('插入数据成功');
+                                       res.status(200).send('成功了');
+                                   }
+                                        }
+                                                    })
+                               }
+                                                    
+                              // res.status(200).send('已存在');
+                               connection.end() ;
+
+                           }
+                     })
+                 }
+    });
+})
+
+   
+
+
+app.use('/testpath', router)
+
+module.exports = app;
