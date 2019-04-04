25faf6380e0a84265e802f698455d21377c06f58
yumign <35503192+yumign@users.noreply.github.com>
2019-04-03 18:40:37 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/pageview', pageview);
+
 
 app.use('/test', router)
 
